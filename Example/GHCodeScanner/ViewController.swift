d92639aecc20944c0172aa74abc07d1b61122155
GoveeHomeApp <app.dev@ihoment.com>
2023-09-22 11:44:39 +0800

    [Add] new module project

---
+++ Example/GHCodeScanner/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GHCodeScanner
+//
+//  Created by GoveeHomeApp on 09/22/2023.
+//  Copyright (c) 2023 GoveeHomeApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
